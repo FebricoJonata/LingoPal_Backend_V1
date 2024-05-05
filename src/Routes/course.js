e4f4b28ee73c58429de0636a942972a1a224e04e
Febrico Jonata <riko9647@gmail.com>
2024-05-05 23:05:39 +0700

    feat: add get progress transction

--- src/Routes/course.js
+++ src/Routes/course.js
@@ -43,4 +43,46 @@ courseRouter.get("/", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/course/progress:
+ *   get:
+ *     summary: Retrieve a list of courses progress
+ *     description: Retrieve a list of courses progress from the database.
+ *     tags:
+ *      - Course
+ *     parameters:
+ *       - in: query
+ *         name: user_id
+ *         schema:
+ *           type: string
+ *         description: Filter users by user id
+ *     responses:
+ *       '200':
+ *         description: A JSON array of courses progress.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ */
+courseRouter.get("/progress", async (req, res) => {
+  try {
+    const { user_id } = req.query;
+    let { data: progress } = await db
+      .from("t_user_course_progress")
+      .select("*")
+      .eq("user_id", user_id);
+
+    return res.status(200).json({
+      status: 200,
+      body: progress,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error: "Internal server error",
+    });
+  }
+});
+
 export default courseRouter;
--- src/Routes/practice.js
+++ src/Routes/practice.js
@@ -39,4 +39,47 @@ practiceRouter.get("/", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/practice/progress:
+ *   get:
+ *     summary: Retrieve a list of practices progress
+ *     description: Retrieve a list of practices progress from the database.
+ *     tags:
+ *      - Practice
+ *     parameters:
+ *       - in: query
+ *         name: user_id
+ *         schema:
+ *           type: string
+ *         description: Filter users by user id
+ *     responses:
+ *       '200':
+ *         description: A JSON array of practices progress.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ */
+practiceRouter.get("/progress", async (req, res) => {
+  try {
+    const { user_id } = req.query;
+
+    let { data: progress } = await db
+      .from("t_user_practice_progress")
+      .select("*")
+      .eq("user_id", user_id);
+
+    return res.status(200).json({
+      status: 200,
+      body: progress,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error: "Internal server error",
+    });
+  }
+});
+
 export default practiceRouter;
